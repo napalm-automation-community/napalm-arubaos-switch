c4e6927fb4f7455dbd08d2331f07010cdf3d8298
Hupe <11520417+hupebln@users.noreply.github.com>
2020-01-09 18:39:15 +0100

    implemented a traceroute method

--- napalm_arubaoss/ArubaOS.py
+++ napalm_arubaoss/ArubaOS.py
@@ -5,6 +5,7 @@ from time import sleep
 
 import requests
 import logging
+import socket
 from netaddr import IPNetwork
 
 from napalm.base.helpers import textfsm_extractor
@@ -585,6 +586,49 @@ class ArubaOSS(NetworkDriver):
                 return True
             else:
                 return False
+
+    def traceroute(self, destination, source='', ttl=255, timeout=2, vrf=''):
+        """Executes traceroute on the device and returns a dictionary with the result.
+
+        :param destination: needed argument
+        :param source: not implemented as not available from device
+        :param ttl: not implemented as not available from device
+        :param timeout: not implemented as not available from device
+        :param vrf: not implemented as not available from device
+        :return: returns a dictionary containing the hops and probes
+        """
+        url = self._api_url + 'trace-route'
+        data = {"destination": {"ip_address": {"version": "IAV_IP_V4", "octets": destination}}}
+        data_post = self._apisession.post(url, json=data)
+
+        if not data_post.status_code == 200:
+            return {'error': 'unknown host {}'.format(destination)}
+
+        ret = {'success': {}}
+        ttl_data = data_post.json().get('ttl_data', [])
+
+        for hop_count in range(len(ttl_data)):
+            ret['success'][hop_count + 1] = {'probes': {}}
+            ttl_probe_data = ttl_data[hop_count].get('ttl_probe_data', [])
+            for probe_count in range(len(ttl_probe_data)):
+                try:
+                    hostname, _, _ = socket.gethostbyaddr(
+                        ttl_probe_data[probe_count].get('gateway', {}).get('ip_address', {}).get('octets', '')
+                    )
+                except socket.herror as _:
+                    hostname = ''
+
+                probe = {
+                    'rtt': float(ttl_probe_data[probe_count]['probe_time_in_millis']),
+                    'ip_address':
+                        ttl_probe_data[probe_count].get('gateway', {}).get('ip_address', {}).get('octets', ''),
+                    'hostname': hostname
+                }
+
+                ret['success'][hop_count + 1]['probes'][probe_count + 1] = probe
+                del probe
+
+        return ret
 
     def close(self):
         """Close device connection and delete sessioncookie."""
