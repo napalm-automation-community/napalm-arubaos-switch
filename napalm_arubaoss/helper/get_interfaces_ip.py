1d9ba652ccd3f1a32aa99ce95aca4df9f4baaf0d
Christian Schirge <schirge@zalando.de>
2021-10-28 16:39:41 +0200

    attempt to harden the method

--- napalm_arubaoss/helper/get_interfaces_ip.py
+++ napalm_arubaoss/helper/get_interfaces_ip.py
@@ -14,18 +14,30 @@ def get_interfaces_ip(self):
     output = {}
     resp = self.connection.get(url)
     if resp.status_code == 200:
-        for address in resp.json().get("ip_address_subnet_element"):
-            iface_name = "VLAN" + str(address["vlan_id"])
+        for address in resp.json().get("ip_address_subnet_element", []):
+            vlan_id = address.get("vlan_id", "")
+            iface_name = "VLAN{vlan_id}".format(vlan_id=str(vlan_id))
+
             if iface_name not in output.keys():
                 output[iface_name] = {}
-            ip = IPNetwork(
-                "{}/{}".format(
-                    address["ip_address"]["octets"], address["ip_mask"]["octets"]
+
+            ip_address = address.get("ip_address", {}).get("octets")
+            ip_mask = address.get("ip_mask", {}).get("octets")
+
+            if ip_address and ip_mask:
+                ip = IPNetwork(
+                    "{ip_address}/{ip_mask}".format(
+                        ip_address=ip_address,
+                        ip_mask=ip_mask
+                    )
                 )
-            )
-            version = "ipv" + str(ip.version)
-            if version not in output[iface_name].keys():
-                output[iface_name][version] = {}
-            output[iface_name][version][str(ip.ip)] = {"prefix_length": ip.prefixlen}
+
+                version = "ipv{ip_version}".format(ip_version=str(ip.version))
+
+                if version not in output[iface_name].keys():
+                    output[iface_name][version] = {}
+                output[iface_name][version][str(ip.ip)] = {
+                    "prefix_length": ip.prefixlen
+                }
 
     return output
