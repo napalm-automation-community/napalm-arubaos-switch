e3b87b7bdf161852c9cc284f59a2fb1ce2960f48
Christian Schirge <schirge@zalando.de>
2021-11-14 10:40:14 +0100

    adding NTP servers added as domain name

--- napalm_arubaoss/helper/get_ntp_servers.py
+++ napalm_arubaoss/helper/get_ntp_servers.py
@@ -8,10 +8,21 @@ logger = logging.getLogger("arubaoss.helper.get_ntp_servers")
 def get_ntp_servers(self):
     """Get NTP servers."""
     " TO-DO: add IPv6 support, currently getting 404 from the API"
-    url = self.connection.config["api_url"] + "config/ntp/server/ip4addr"
-    resp = self.connection.get(url)
-    if resp.status_code == 200:
-        output = {}
-        for server in resp.json().get("ntpServerIp4addr_element"):
+    base_url = self.connection.config["api_url"]
+    ipv4_url = f"{base_url}config/ntp/server/ip4addr"
+    name_url = f"{base_url}config/ntp/server-name/ASCII-STR"
+
+    ipv4_resp = self.connection.get(ipv4_url)
+    name_resp = self.connection.get(name_url)
+
+    output = {}
+
+    if ipv4_resp.status_code == 200:
+        for server in ipv4_resp.json().get("ntpServerIp4addr_element"):
             output[server["ip4addr"]["ip4addr_value"]] = {}
+
+    if name_resp.status_code == 200:
+        for server in name_resp.json().get("ntpServerNameASCIISTR_element"):
+            output[server["ASCII-STR"]["ASCII-STR_value"]] = {}
+
         return output
